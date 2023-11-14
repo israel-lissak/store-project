f069457ed65c7079c5c4ad08a2fb83a14856eb45
yonathan <yonathan.eisenberg@gmail.com>
2023-11-14 16:52:22 +0200

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import Product from './pages/Product';
-import Category from './pages/Category';
-import Home from './pages/Home';
+// import Product from './components/Product';
+// import Category from './components/Category';
+import Home from './components/Home';
+import ShoppingCart from './components/Cart';
 
 function App() {
   
@@ -18,15 +19,18 @@ function App() {
         <a href="/category">cagegory</a>
       |
         <a href="/product">product</a>
+      |
+        <a href='/cart'>Shopping Cart</a>
      </nav>
 
      <h1>welcome to our store</h1>
 
       <BrowserRouter>
         <Routes>
-          <Route path="/product/:id" element={<Product />} />
+          {/* <Route path="/product/:id" element={<Product />} /> */}
           <Route path="/home" element={<Home />} />
-          <Route path="/category" element={<Category />} />
+          {/* <Route path="/category" element={<Category />} /> */}
+          <Route path='/cart' element={<ShoppingCart />}/>
         </Routes>
       </BrowserRouter>
 
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,60 +1,77 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect} from 'react';
 
-function ShoppingCart() {
-  localStorage.setItem('cart', undefined)
-  const [cartData, setCartData] = useState(
-    JSON.parse(localStorage.getItem('cart')) || []
-  );
-  if(!cartData.length){
-    console.log(45);
-    setCartData(
-      localStorage.setItem(
-        'cart', JSON.stringify(
-          [
-            {
-              id:'no id',
-              name:'the cart is empty',
-              price:0,
-              quantity:0
-            }
-          ]
-        )
-      )
-    )
+function ShoppingCart() {  
+  console.log(1)
+  localStorage.setItem('cart',JSON.stringify([
+    {id:'3', quantity:4},
+    {id:'78', quantity:45},
+    {id:'4', price:78, name:'67', quantity:45}
+  ]))
+  console.log(2);
+  
+  let cart = []
+  // let quantity = {}
+  if (localStorage.getItem('logIn')==='true') {
+    const user = localStorage.getItem('userID');
+    const arrOfProducts = await getFromUserInDB(user);
+    cart = await fromProductIDToListOfProducts(arrOfProducts);
+  }else {
+    const arrOfProducts = localStorage.getItem('cart')
+    // cart = await fromProductIDToListOfProducts(arrOfProducts)
+      cart = arrOfProducts
+      console.log(cart);
+      console.log(3);
+  }
+  const [cartData, setCartData] = useState(cart || []);
+  console.log(4);
+  console.log(cartData);
+  
+  
+  async function getFromUserInDB(userID:string) {
+    const jsonData = await fetch(`http://localhost:3000/cart/${userID}`)
+    const data = await jsonData.json()
+    return data
+  }
+  
+  const addOne = async (productID:string, quantity:number) => {
+    const updatedCart = cartData.map((item) => {
+      if (item.id === productID) {
+        return { ...item, quantity: quantity+1 };
+      }
+      return item;
+    });
+    setCartData(updatedCart);
+    localStorage.setItem('cart', JSON.stringify(updatedCart))
   };
 
-  const updateQuantity = (productId:string, newQuantity:number) => {
-    console.log(productId);
+  const reduceOne = async (productID:string, quantity:number) => {
     const updatedCart = cartData.map((item) => {
-      if (item.id === productId) {
-        return { ...item, quantity: newQuantity };
+      if (item.id === productID) {
+        return { ...item, quantity: quantity-1 };
       }
       return item;
     }).filter((item) => item.quantity > 0); // Remove items with quantity zero.
     setCartData(updatedCart);
-    localStorage.setItem('cart', JSON.stringify(updatedCart));
-    console.log(localStorage.getItem('cart'));
+    localStorage.setItem('cart', JSON.stringify(updatedCart))
   };
 
   const removeItem = (productId:string) => {
-    console.log(productId);
     const updatedCart = cartData.filter((item)=>
         item.id !== productId
     )
     setCartData(updatedCart);
-    localStorage.setItem('cart', updatedCart)
-    console.log(localStorage.getItem('cart'));
+    localStorage.setItem('cart', JSON.stringify(updatedCart))
   };
 
   const clearCart = () => {
-    setCartData([{
+    const emptyCart = [{
       id:'no id',
       name:'the cart is empty',
       price:0,
       quantity:0
-    }]);
-    localStorage.setItem('cart', cartData);
-    console.log(localStorage.getItem('cart'));
+    }]
+    setCartData(emptyCart);
+    localStorage.setItem('cart', JSON.stringify(emptyCart));
   };
 
   const buy = ()=>{
@@ -67,8 +84,20 @@ function ShoppingCart() {
     return cartData.reduce((total, item) => total + item.price * item.quantity, 0);
   };
 
+    
+  async function fromProductIDToListOfProducts(arr:{product_id:string}[]){
+    const list = []
+    for (let i = 0; i < arr.length; i++) {
+      const element = arr[i].product_id;
+      const jsonItem = await fetch(`http://localhost:3000/shop/${element}`);
+      const listItem = await jsonItem.json();
+      list.push(listItem);
+    }
+    return list
+  }
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(cartData));
+    console.log('render');
   }, [cartData]);
 
   return (
@@ -84,14 +113,14 @@ function ShoppingCart() {
           </tr>
         </thead>
         <tbody>
-          {cartData.map((item) => (
+          {cartData.map((item) => (            
             <tr key={item.id}>
-              <td><h4>{item.name}</h4></td>
+              <td><h4>{item.title}</h4></td>
               <td>${item.price.toFixed(2)}</td>
               <td>
-                <button onClick={() => updateQuantity(item.id, item.quantity - 1)}>-</button>
-                {item.quantity}
-                <button onClick={() => updateQuantity(item.id, item.quantity + 1)}>+</button>
+                <button onClick={() => reduceOne(item.id, item.quantity)}>-</button>
+                  {item.quantity}
+                <button onClick={() => addOne(item.id, item.quantity)}>+</button>
               </td>
               <td>${(item.price * item.quantity).toFixed(2)}</td>
               <td>
@@ -111,3 +140,4 @@ function ShoppingCart() {
 }
 
 export default ShoppingCart;
+
